bbf2186c1f52ed417e161826567ec3c1351666e9
Nazar Drozd <nazardrozd228@gmail.com>
2023-01-30 17:59:30 +0200

    UI

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include "server/interface/container.h"
 #include "server/interface/router.h"
+#include "ServerUI.h"
 
 int main(int argc, char* argv[]) {
   interface::Container container;
   container.setRouter(new interface::Router());
+ /* myUI ui;
+  ui.Run();*/
   // Run application with Router as Request Handler Factory
   return container.run(argc, argv);
 }
