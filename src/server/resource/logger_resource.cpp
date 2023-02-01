6827e84e3cd18e087e4ca13b8c6ae8307fb1b3f4
andrewgll <andrii.hladkyi.teacher@gmail.com>
2023-02-01 13:04:56 +0200

    Small changes

--- src/server/resource/logger_resource.cpp
+++ src/server/resource/logger_resource.cpp
@@ -13,17 +13,7 @@ namespace resource {
 LoggerResource::LoggerResource() : AbstractResource() {}
 
 void LoggerResource::handle_get(Poco::Net::HTTPServerRequest &request,
-                                Poco::Net::HTTPServerResponse &response) {
-  std::string id = getQueryParameter("id", false);
-  if (id == "") {
-    auto device = dbService.loadDB();
-    Poco::JSON::Stringifier::condense(device, response.send());
-
-  } else {
-    auto device = dbService.findDevice(id);
-    Poco::JSON::Stringifier::condense(device, response.send());
-  }
-}
+                                Poco::Net::HTTPServerResponse &response) {}
 void LoggerResource::handle_options(Poco::Net::HTTPServerRequest &,
                                     Poco::Net::HTTPServerResponse &response) {
   response.set("Allow", "GET, OPTIONS");
